e6250ef38290ed84cb9a4c387e4c7d523402f81d
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-12 17:06:46 +0800

    sbp1055

--- P1055/main.cpp
+++ P1055/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a,b,c,d,e,f,g,h,i,identifier;
-    char j;
-    scanf("%1d-%1d%1d%1d-%1d%1d%1d%1d%1d-j",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
-    identifier = (a*1+b*2+c*3+d*4+e*5+f*6+g*7+h*8+i*9)%11;
-    if (identifier == j) {
+    int a,b,c,d,e,f,g,h,i,int_identifier;
+    char char_identifier;
+    scanf("%1d-%1d%1d%1d-%1d%1d%1d%1d%1d-%c",&a,&b,&c,&d,&e,&f,&g,&h,&i,&char_identifier);
+    char real_identifier = (a*1+b*2+c*3+d*4+e*5+f*6+g*7+h*8+i*9)%11;
+    if (real_identifier == 10) {
+        real_identifier = 0x58;
+    }else {
+        real_identifier = real_identifier + '0';
+    }
+    if (char_identifier == real_identifier) {
         cout << "Right";
     }
 
     else {
-        printf(,a,b,c,d,e,f,g,h,i,identifier)
+        printf("%d-%d%d%d-%d%d%d%d%d-%c",a,b,c,d,e,f,g,h,i,real_identifier);
     }
     return 0;
 }
