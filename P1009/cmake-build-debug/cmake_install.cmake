a887f8d3cf2125d515c62df5e2e519d481629539
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-15 07:25:03 +0800

    1

--- P1009/cmake-build-debug/cmake_install.cmake
+++ P1009/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/kcalb/CLionProjects/P1009
+# Install script for directory: C:/Users/kcalb/Documents/luogu/P1009
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/kcalb/CLionProjects/P1009/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kcalb/Documents/luogu/P1009/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- P1009/main.cpp
+++ P1009/main.cpp
@@ -3,53 +3,91 @@
 #include <cmath>
 using namespace std;
 
-vector<int> high_def_add(vector<int> a, vector<int> b) {
+vector<int> high_def_add(const vector<int> a, const vector<int> b) {
     vector<int> a_reverse_temp = {};
     vector<int> b_reverse_temp = {};
     vector<int> result_reverse_temp = {};
     vector<int> result = {};
 
     for(int i = a.size() - 1; i >= 0; i--) {
-        a_reverse_temp.push_back(i);
-    }
+        a_reverse_temp.push_back(a[i]);
+    }                                                   //反转数组a
     for(int i = b.size() - 1; i >= 0; i--) {
-        b_reverse_temp.push_back(i);
-    }
-    if(a_reverse_temp.size() < b_reverse_temp.size()) {
-        for(int i = 0; i < a.size(); i++) {
-            result_reverse_temp.push_back(a_reverse_temp[i] + b_reverse_temp[i]);
+        b_reverse_temp.push_back(b[i]);
+    }                                                   //反转数组b
+        for(int i = 0; i < min(a.size(), b.size()); i++) {
+            int temp = a_reverse_temp[i] + b_reverse_temp[i];
+            if (temp >= 10) {
+                result_reverse_temp.push_back(temp - 10);
+                b_reverse_temp[i + 1] += 1;
+            } else {
+                result_reverse_temp.push_back(temp);
+            }
+        }                                               //将a和b的每一位相加
+        if(a.size() < b.size()) {
+            for(int i = a.size(); i < b.size(); i++) {
+                result_reverse_temp.push_back(b_reverse_temp[i]);
+            }
+        } else {
+            for(int i = b.size(); i < a.size(); i++) {
+                result_reverse_temp.push_back(b_reverse_temp[i]);
+            }
         }
-        for(int i = a.size(); i < b.size(); i++) {
-            result_reverse_temp.push_back(b_reverse_temp[i]);
-        }
-    } else {
-        for(int i = 0; i < b.size(); i++) {
-            result_reverse_temp.push_back(b_reverse_temp[i] + a_reverse_temp[i]);
-        }
-        for(int i = b.size(); i < a.size(); i++) {
-            result_reverse_temp.push_back(a_reverse_temp[i]);
-        }
-    }
     for(int i : result_reverse_temp) {
-        result.push_back(i);
+        result.insert(result.begin(), i);
     }
     return result;
 };
 
+vector<int> high_def_multiply(const vector<int> a, const vector<int> b) {
+    vector<int> a_reverse_temp = {};
+    vector<int> b_reverse_temp = {};
+    vector<int> result_reverse_temp = {};
+    vector<int> result = {};
+
+    for(int i = a.size() - 1; i >= 0; i--) {
+        a_reverse_temp.push_back(a[i]);
+    }                                                   //反转数组a
+    for(int i = b.size() - 1; i >= 0; i--) {
+        b_reverse_temp.push_back(a[i]);
+    }                                                   //反转数组b
+    for(int i = 0; i < min(a.size(), b.size()); i++) {
+        int temp;
+        temp = a_reverse_temp[i] * b_reverse_temp[i];
+        if(temp >= 10) {
+            result_reverse_temp.push_back(temp % 10);
+            result_reverse_temp.push_back(temp / 10);
+        } else {
+            result_reverse_temp.push_back(temp);
+        }
+    }                                                   //将a和b的每一位相乘
+    if(a.size() < b.size()) {
+        for(int i = a.size(); i < b.size(); i++) {
+            result_reverse_temp.push_back(b_reverse_temp[i]);
+        }
+    } else {
+        for(int i = b.size(); i < a.size(); i++) {
+            result_reverse_temp.push_back(b_reverse_temp[i]);
+        }
+    }
+    return result;
+}
+
 int main()
 {
     vector<int> a = {1,2,3,4,5};
-    vector<int> b = {1,2,3};
+    vector<int> b = {5,4,3,2,1};
 
+    vector<int> result = high_def_add(a,b);
+    vector<int> multiply = high_def_multiply(a,b);
 
-    vector<int> result = high_def_add(a, b);
     for(int i : result) {
-        printf("%d ",i);
+        printf("%d",i);
     }
 
-//    int n;
-//    cin >> n;
-//    vector<int> number = {};
+    for(int i : multiply) {
+        printf("%d",i);
+    }
 
     return 0;
 }
