dccc12c71a10e8802e20606b77994ce2175530d7
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-15 09:44:23 +0800

    1

--- P1009/main.cpp
+++ P1009/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <regex>
 using namespace std;
 
 vector<int> high_def_add(const vector<int> a, const vector<int> b) {
@@ -8,6 +9,7 @@ vector<int> high_def_add(const vector<int> a, const vector<int> b) {
     vector<int> b_reverse_temp = {};
     vector<int> result_reverse_temp = {};
     vector<int> result = {};
+    int one = 0;
 
     for(int i = a.size() - 1; i >= 0; i--) {
         a_reverse_temp.push_back(a[i]);
@@ -16,10 +18,11 @@ vector<int> high_def_add(const vector<int> a, const vector<int> b) {
         b_reverse_temp.push_back(b[i]);
     }                                                   //反转数组b
         for(int i = 0; i < min(a.size(), b.size()); i++) {
-            int temp = a_reverse_temp[i] + b_reverse_temp[i];
+            int temp = a_reverse_temp[i] + b_reverse_temp[i] + one;
+            one = 0;
             if (temp >= 10) {
                 result_reverse_temp.push_back(temp - 10);
-                b_reverse_temp[i + 1] += 1;
+                one += 1;
             } else {
                 result_reverse_temp.push_back(temp);
             }
@@ -44,6 +47,7 @@ vector<int> high_def_multiply(const vector<int> a, const vector<int> b) {
     vector<int> b_reverse_temp = {};
     vector<int> result_reverse_temp = {};
     vector<int> result = {};
+    int temp = 0;
 
     for(int i = a.size() - 1; i >= 0; i--) {
         a_reverse_temp.push_back(a[i]);
@@ -52,14 +56,7 @@ vector<int> high_def_multiply(const vector<int> a, const vector<int> b) {
         b_reverse_temp.push_back(a[i]);
     }                                                   //反转数组b
     for(int i = 0; i < min(a.size(), b.size()); i++) {
-        int temp;
-        temp = a_reverse_temp[i] * b_reverse_temp[i];
-        if(temp >= 10) {
-            result_reverse_temp.push_back(temp % 10);
-            result_reverse_temp.push_back(temp / 10);
-        } else {
-            result_reverse_temp.push_back(temp);
-        }
+        result_reverse_temp.push_back(a_reverse_temp[i] * b_reverse_temp[i]);
     }                                                   //将a和b的每一位相乘
     if(a.size() < b.size()) {
         for(int i = a.size(); i < b.size(); i++) {
@@ -70,11 +67,20 @@ vector<int> high_def_multiply(const vector<int> a, const vector<int> b) {
             result_reverse_temp.push_back(b_reverse_temp[i]);
         }
     }
+    for(int i = 0; i < result_reverse_temp.size(); i++ ) {
+        result_reverse_temp[i] += temp;
+        int temp = 0;
+        if(i >= 10) {
+
+        }
+    }
+    for(int i : result_reverse_temp) {
+        result.insert(result.begin(), i);
+    }
     return result;
 }
 
-int main()
-{
+int main(){
     vector<int> a = {1,2,3,4,5};
     vector<int> b = {5,4,3,2,1};
 
