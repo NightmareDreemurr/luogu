09e6ddcda1bdff4be7cc5c2a7e0473ddc08848ce
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-28 23:20:43 +0800

    1

---
+++ C++/P2550/cmake-build-debug/CMakeFiles/P2550.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P2550.dir/main.cpp.obj"
+  "CMakeFiles/P2550.dir/main.cpp.obj.d"
+  "P2550.exe"
+  "P2550.exe.manifest"
+  "P2550.pdb"
+  "libP2550.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P2550.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- C++/P2550/main.cpp
+++ C++/P2550/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int ticket_number;
+    cin >> ticket_number;
+    vector<int> award = {0,0,0,0,0,0,0};
+
+    set<int> winning_numbers;
+    for(int i = 0; i < 7; i++) {
+        int temp;
+        cin >> temp;
+        winning_numbers.insert(temp);
+    }
+
+
+    vector<set<int>> temp_ticket;
+    temp_ticket.resize(ticket_number);
+
+    for(int i = 0; i < ticket_number; i++) {
+        for(int j = 0; j < 7; j++) {
+            int temp;
+            cin >> temp;
+            temp_ticket[i].insert(temp);
+        }
+    }
+    for(int i = 0; i < ticket_number; i++) {
+        int rating = 0;
+        for(int temp : temp_ticket[i]) {
+            if(winning_numbers.count(temp)) {
+                rating ++;
+            }
+        }
+        if(rating != 0) {
+            award[7-rating] += 1;
+        }
+    }
+    for(int i : award) {
+        cout << i << " ";
+    }
     return 0;
 }
