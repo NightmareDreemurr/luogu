12545a9596ec03d8528ddedcec356256f754d891
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-29 19:49:50 +0800

    1

---
+++ C++/P5730/cmake-build-debug/CMakeFiles/P5730.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P5730.dir/main.cpp.obj"
+  "CMakeFiles/P5730.dir/main.cpp.obj.d"
+  "P5730.exe"
+  "P5730.exe.manifest"
+  "P5730.pdb"
+  "libP5730.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P5730.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- C++/P5730/main.cpp
+++ C++/P5730/main.cpp
@@ -4,33 +4,34 @@
 using namespace std;
 int main() {
     map<int,vector<string>> number_map;
-    number_map[0] = {"xxx","x.x","x.x","x.x","xxx"};
-    number_map[1] = {"..x","..x","..x","..x","..x"};
-    number_map[2] = {"xxx","..x","xxx","x..","xxx"};
-    number_map[3] = {"xxx","..x","xxx","..x","xxx"};
-    number_map[4] = {"x.x","x.x","xxx","..x","..x"};
-    number_map[5] = {"xxx","x..","xxx","..x","xxx"};
-    number_map[6] = {"xxx","x..","xxx","x.x","xxx"};
-    number_map[7] = {"xxx","..x","..x","..x","..x"};
-    number_map[8] = {"xxx","x.x","xxx","x.x","xxx"};
-    number_map[9] = {"xxx","x.x","xxx","..x","xxx"};
+    number_map[0] = {"XXX","X.X","X.X","X.X","XXX"};
+    number_map[1] = {"..X","..X","..X","..X","..X"};
+    number_map[2] = {"XXX","..X","XXX","X..","XXX"};
+    number_map[3] = {"XXX","..X","XXX","..X","XXX"};
+    number_map[4] = {"X.X","X.X","XXX","..X","..X"};
+    number_map[5] = {"XXX","X..","XXX","..X","XXX"};
+    number_map[6] = {"XXX","X..","XXX","X.X","XXX"};
+    number_map[7] = {"XXX","..X","..X","..X","..X"};
+    number_map[8] = {"XXX","X.X","XXX","X.X","XXX"};
+    number_map[9] = {"XXX","X.X","XXX","..X","XXX"};
 
 
-
-    int n = 0;
+    int n;
     cin >> n;
-    vector<int> numbers;
+    vector<char> numbers;
+
+    getchar();
+    getchar();
 
     for(int i = 0; i < n; i++) {
-        int temp = 0;
-        temp = getchar() - '0';
+        char temp = getchar();
         numbers.push_back(temp);
     }
     numbers.shrink_to_fit();
 
-    for(int line = 0; line < 5; line ++) {
+     for(int line = 0; line < 5; line ++) {
         for(int i = 0; i < n; i++) {
-            int temp = numbers[i];
+            int temp = numbers[i] - '0';
             cout << number_map[temp][line];
             if(i != n - 1) {
                 cout << ".";
@@ -38,6 +39,7 @@ int main() {
         }
         cout << endl;
     }
+
 
 
 
