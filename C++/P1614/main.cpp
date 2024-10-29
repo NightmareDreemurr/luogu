090ffed7aecf9d61c4d882d27c7f93dc8a6efd6b
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-29 23:10:37 +0800

    1

--- C++/P1614/main.cpp
+++ C++/P1614/main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long long a, b;
+    vector<long long> num_list;
+    long long answer = 2147483647;
+    cin >> a >> b;
+    for (long long i = 0; i < a; i++) {
+        long long temp;
+        cin >> temp;
+        num_list.push_back(temp);
+    }
+    for (long long i = 0; i < a - b; i++) {
+        long long temp = 0;
+        for (long long j = i; j < i + b; j++) {
+            temp += num_list[j];
+        }
+        answer = min(answer, temp);
+    }
+
+    cout << answer;
+
     return 0;
 }
