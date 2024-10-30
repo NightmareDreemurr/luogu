af3337fe9b589e11f7b074758b1a62a79f1a5eb9
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-10-30 11:12:51 +0800

    1

--- C++/P5731/main.cpp
+++ C++/P5731/main.cpp
@@ -2,5 +2,60 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int n2 = n * n;
+    int arr[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    int count = 0;
+    int direction = 1;
+    int hang = 0, lie = -1;
+    while(count < n2){
+        switch(direction % 4) {
+            case 1:
+                if((lie+1 < n) && arr[hang][lie+1] == 0) {
+                    lie++;
+                    count++;
+                } else {
+                    direction++;
+                }
+                break;
+            case 2:
+                if((hang+1 < n) && arr[hang+1][lie] == 0) {
+                    hang++;
+                    count++;
+                } else {
+                    direction++;
+                }
+                break;
+            case 3:
+                if((lie-1 >= 0) && arr[hang][lie-1] == 0) {
+                    lie--;
+                    count++;
+                } else {
+                    direction++;
+                }
+                break;
+            case 0:
+                if((hang-1 < n) && arr[hang-1][lie] == 0) {
+                    hang--;
+                    count++;
+                } else {
+                    direction++;
+                }
+                break;
+        }
+        arr[hang][lie] = count;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d", arr[i][j]);
+        }
+        cout << endl;
+    }
     return 0;
 }
---
+++ C++/P5732/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> yhsj;
+    yhsj.resize(n);
+    yhsj[0].push_back(1);
+    yhsj[1].push_back(1);
+    yhsj[1].push_back(1);
+
+    for (int i = 1; i < n; i++) {
+        int first_number_index = 0;
+        if(i != n-1){
+            yhsj[i+1].push_back(1);
+        }
+        while((first_number_index + 1 < yhsj[i].size()) && i != n-1) {
+            yhsj[i+1].push_back(yhsj[i][first_number_index] + yhsj[i][first_number_index+1]);
+            first_number_index ++;
+        }
+        if(i != n-1){
+            yhsj[i+1].push_back(1);
+        }
+    }
+    for (auto & i : yhsj) {
+        for (int j : i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
