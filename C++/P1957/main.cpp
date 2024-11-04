098a5db2b8ae714fefab9875b014479a9844fd88
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-11-04 23:00:35 +0800

    1

---
+++ C++/P1957/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    getchar();
+    getchar();
+
+    string next_problem;
+    char temp;
+
+
+    return 0;
+}
