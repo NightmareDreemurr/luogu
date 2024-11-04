eba66e4728e91082d391f31a8da8fe7aafde2344
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-11-04 23:00:20 +0800

    1

---
+++ C++/P1205/cmake-build-debug/CMakeFiles/P1205.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1205.dir/main.cpp.obj"
+  "CMakeFiles/P1205.dir/main.cpp.obj.d"
+  "P1205.exe"
+  "P1205.exe.manifest"
+  "P1205.pdb"
+  "libP1205.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1205.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
